94658739b87c32c34cb7d2d96ad8d5280783b289
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-06-11 22:26:26 +0300

    Added gulp-ruby-sass package and established compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
+	sass = require('gulp-ruby-sass'), //variant 1 author
+	compass = require('gulp-compass'), //variant 2
 	concat  = require('gulp-concat');
 
 var jsSources = [
@@ -10,6 +12,7 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
 	gulp.src('components/coffe/*.coffee')
@@ -23,4 +26,27 @@ gulp.task('js', function() {
 		.pipe(concat('scripts.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+//variant 1
+// gulp.task('compass', function() {
+// 	gulp.src(sassSources)
+// 		.pipe(compass({
+// 			comments: true,
+// 			sass: 'components/sass',
+// 			images: 'builds/development/images',
+// 			style: 'expanded'
+// 		})
+// 		.on('error', gutil.log ))
+// 		.pipe(gulp.dest('builds/development/css'))
+// });
+
+//variant 2
+gulp.task('sass', function() {
+   return sass(sassSources, {
+     compass: true,
+     lineNumbers: true,
+     style: 'expanded'
+   }).on('error', gutil.log)
+   .pipe(gulp.dest('builds/development/css'))
 });
