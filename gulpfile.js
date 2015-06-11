cda7b8912ed05581016ece3c40e6bd92479607ef
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-06-11 18:42:20 +0300

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
 	concat  = require('gulp-concat');
 
 var jsSources = [
@@ -20,5 +21,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('scripts.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
