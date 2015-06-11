ab6dc691e1891ad67cd57107303e5df7569a8646
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-06-11 13:23:21 +0300

    Added coffee script task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.log('Awesome!');
+gulp.task('coffee', function() {
+	gulp.src('components/coffe/*.coffee')
+		.pipe(coffee({bare: true}).on('error', gutil.log ))
+		.pipe(gulp.dest('components/scripts'))
+
 });
