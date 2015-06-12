f273edb02036e8e374cb877a5faf79cf3292e5e4
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-06-12 17:34:20 +0300

    Added html and json file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,8 @@ var jsSources = [
 	'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -61,6 +63,9 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['sass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
+
 });
 
 gulp.task('connect', function() {
@@ -70,4 +75,14 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('default', ['coffee', 'js', 'sass', 'connect', 'watch']);
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+	.pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+	.pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'sass', 'connect', 'watch']);
