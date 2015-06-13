13020b08b94d64a3e5b982f5f19707963b3d6e89
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-06-13 15:36:14 +0300

    Minified production json files

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var gulp = require('gulp'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
 	minifyHtml = require('gulp-minify-html'),
+	jsonminify = require('gulp-jsonminify'),
 	concat  = require('gulp-concat');
 
 var env,
@@ -86,7 +87,7 @@ gulp.task('watch', function() {
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['sass']);
 	gulp.watch('builds/development/*.html', ['html']);
-	gulp.watch(jsonSources, ['json']);
+	gulp.watch('builds/development/js/*.json', ['json']);
 
 });
 
@@ -105,7 +106,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('json', function() {
-	gulp.src(jsonSources)
+	gulp.src('builds/development/js/*.json')
+	.pipe(gulpif(env === 'production', jsonminify()))
+	.pipe(gulpif(env === 'production', gulp.dest('builds/production/js/')))
 	.pipe(connect.reload())
 });
 
