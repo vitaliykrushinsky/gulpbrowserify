7e11cc85f1bef117d9feebf918d07c89dd9eb1f7
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2015-06-13 15:18:15 +0300

    Minified production html files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
+	minifyHtml = require('gulp-minify-html'),
 	concat  = require('gulp-concat');
 
 var env,
@@ -84,7 +85,7 @@ gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['sass']);
-	gulp.watch(htmlSources, ['html']);
+	gulp.watch('builds/development/*.html', ['html']);
 	gulp.watch(jsonSources, ['json']);
 
 });
@@ -97,7 +98,9 @@ gulp.task('connect', function() {
 });
 
 gulp.task('html', function() {
-	gulp.src(htmlSources)
+	gulp.src('builds/development/*.html')
+	.pipe(gulpif(env === 'production', minifyHtml()))
+	.pipe(gulpif(env === 'production', gulp.dest(outputDir)))
 	.pipe(connect.reload())
 });
 
